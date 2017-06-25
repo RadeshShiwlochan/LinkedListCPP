5854e208356e91c7f23db135eba527b3984b380d
Radesh Shiwlochan <RadeshShiwlochan@Radeshs-MBP.fios-router.home>
2017-06-25 00:35:50 -0400

    implements Node class

--- node.cpp
+++ node.cpp
@@ -5,12 +5,41 @@ class Node {
 	private:
 		int data;
 		Node* next;
+		Node* previous;
 
 	public:
 		Node(int);
 		~Node();
-		Node(const Node&); //copy constructor
+		Node(const Node* &); //copy constructor
 		int getData();
-		void setData(int data);
+		void setData(int);
+		void setNext(Node* &);
+		void setPrevious(Node* &);
 };
+
+Node::Node(int nodeData) {
+	data     = nodeData;
+	next     = nullptr; 
+	previous = nullptr;
+}
+
+Node::~Node() {
+	delete next;
+	delete previous;
+}
+
+Node::Node(const Node* &node2) {
+	data     = node2->data;
+	next     = node2->next;
+	previous = node2->previous;
+}
+
+int Node::getData() { return data; }
+
+void Node::setData(int nodeData) { data = nodeData; }
+
+void Node::setNext(Node* &node2) { next = node2->next; }
+
+void Node::setPrevious(Node* &node2) { node2 = node2->previous; }
+
 
